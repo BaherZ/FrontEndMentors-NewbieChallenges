5efd766b27a8e215e355ccc407b9886685c77c7f
BaherZ <mbaher18@gmail.com>
2021-04-05 22:13:53 +0200

    email validation

---
+++ base-apparel-coming-soon-master/index.js
@@ -0,0 +1,28 @@
+const errorIcon = document.getElementById('error-icon')
+const errorLabel = document.getElementById('error-label')
+const submitButton = document.getElementById('submit')
+const email = document.getElementById('email')
+
+function isEmailValid(email){
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const isMatch = regex.test(String(email).toLowerCase())
+    console.log("in function, isMatch is: ",isMatch)
+    return isMatch
+}
+
+submitButton.addEventListener('click',(e)=>{
+    e.preventDefault()
+    value = email.value
+    emailIsValid = isEmailValid(value)
+    console.log(emailIsValid)
+    if(!emailIsValid){
+        errorLabel.style.display = "initial"
+        errorIcon.style.display = "initial"
+        email.style.border = "2px solid hsl(0, 93%, 68%)"
+    }else{
+        errorLabel.style.display = "none"
+        errorIcon.style.display = "none"
+        email.style.border = "1px solid hsl(0, 36%, 70%)"
+    }
+})
