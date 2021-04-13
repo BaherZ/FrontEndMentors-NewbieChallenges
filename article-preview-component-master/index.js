f8e5bc9659873e1c75ac943dc41b72d26d5dc780
BaherZ <mbaher18@gmail.com>
2021-04-13 23:29:52 +0200

    mobile responsitivity

--- article-preview-component-master/index.js
+++ article-preview-component-master/index.js
@@ -1,12 +1,40 @@
-const toolTipButton = document.querySelector('button')
-console.log(toolTipButton)
+const width = document.body.clientWidth
+console.log(document.body.clientWidth)
 
-const tooltip = document.getElementById('tooltip')
+if(width>870){
+    let shareButton = document.getElementsByClassName('button')[0]
+    const tooltip = document.getElementById('tooltip')
+    
+    shareButton.addEventListener("click",()=>{
+        if(tooltip.style.display === "flex"){
+            tooltip.style.display = "none"; 
+        } 
+        else {
+            tooltip.style.display = "flex"; 
+        }
+    })
+}
+else{
+    let openShareButton = document.getElementsByClassName('button')[0]
+    let closeShareButton = document.getElementsByClassName('close-button')[0]
 
-toolTipButton.addEventListener("click",()=>{
-    if(tooltip.style.display === "flex"){
-        tooltip.style.display = "none"; 
-    } else {
-        tooltip.style.display = "flex"; 
-    }
-})
+    const meta = document.getElementsByClassName("meta")[0]
+    const mobileShare = document.getElementsByClassName("mobile-share")[0]
+    console.log(meta,mobileShare)
+    openShareButton.addEventListener("click",()=>{
+        
+        //hide the meta div
+        meta.style.display = "none"; 
+        
+        //show the mobile-share div
+        mobileShare.style.display = "flex"
+
+    })
+
+    closeShareButton.addEventListener("click",()=>{
+        meta.style.display = "flex"
+
+        mobileShare.style.display = "none"
+    })
+}
+
