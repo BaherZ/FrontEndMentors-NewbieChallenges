14478899bcbeabf61a4fa7dc376f6690ed5ac778
BaherZ <mbaher18@gmail.com>
2021-04-13 06:22:12 +0200

    add javascript to make tooltip appear on click

---
+++ article-preview-component-master/index.js
@@ -0,0 +1,12 @@
+const toolTipButton = document.getElementById('button')
+console.log(toolTipButton)
+
+const tooltip = document.getElementById('tooltip')
+
+toolTipButton.addEventListener("click",()=>{
+    if(tooltip.style.display === "flex"){
+        tooltip.style.display = "none"; 
+    } else {
+        tooltip.style.display = "flex"; 
+    }
+})
