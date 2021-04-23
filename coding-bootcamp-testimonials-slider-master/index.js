c6ee4d43af8f9dd3e5d104b4e7df7b3d2632a920
BaherZ <mbaher18@gmail.com>
2021-04-23 06:13:13 +0200

    button positioning

--- coding-bootcamp-testimonials-slider-master/index.js
+++ coding-bootcamp-testimonials-slider-master/index.js
@@ -13,16 +13,12 @@ const updateData = ()=>{
     console.log(state)
     if(state==0){
         //show tanya
-        console.log(image.src)
         image.src = "./images/image-tanya.jpg"
-        // image.srcset = "./images/image-tanya.jpg"
         document.getElementsByClassName('content-tanya')[0].style.display = 'flex'
         document.getElementsByClassName('content-john')[0].style.display = 'none'
     }else{
         //show john
-        console.log(image.src)
         image.src = "./images/image-john.jpg"
-        // image.srcset = "./images/image-john.jpg"
         document.getElementsByClassName('content-john')[0].style.display = 'flex'
         document.getElementsByClassName('content-tanya')[0].style.display = 'none'
     }
